d961311b14c87e87df63b66d5a578c62f7d773c3
luozi01 <potterx20a@gmail.com>
2018-04-25 15:12:43 -0400

    Second commit

--- examples/Symbolic_engine/TGPTest.java
+++ examples/Symbolic_engine/TGPTest.java
@@ -3,6 +3,7 @@ package Symbolic_engine;
 import com.zluo.ga.utils.Observation;
 import com.zluo.tgp.gp.TGPChromosome;
 import com.zluo.tgp.program.Operator;
+import com.zluo.tgp.solver.FitnessCalc;
 import com.zluo.tgp.solver.TGPSolver;
 import com.zluo.tgp.solver.TreeGP;
 
@@ -72,6 +73,30 @@ public class TGPTest {
         List<T> list = new LinkedList<>();
         Collections.addAll(list, items);
         return list;
+    }
+
+    private static class TabulatedFunctionFitness implements FitnessCalc {
+
+        private List<Observation> targets = new LinkedList<>();
+
+        TabulatedFunctionFitness(List<Observation> targets) {
+            this.targets.addAll(targets);
+        }
+
+        @Override
+        public double fitness(TGPChromosome expression) {
+            double diff = 0;
+
+            for (Observation o : this.targets) {
+                for (int i = 0; i < o.inputCount(); i++) {
+                    expression.getManager().setVariable(o.getTextInput(i), o.getInput(i));
+                }
+                double targetValue = o.getOutput(0);
+                double calculatedValue = expression.eval();
+                diff += Math.pow(targetValue - calculatedValue, 2);
+            }
+            return diff;
+        }
     }
 }
 
